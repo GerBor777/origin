45cca7d9885e5043cdcaa8ef519f606eb7ee9a09
GerBor777 <germ3245@gmail.com>
2025-06-12 00:47:37 +0300

    Домашнее задание: QtConcurrent (только исходные файлы)

---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QMutex>
+#include <QtConcurrent>
+#include <QFuture>
+
+#include "ExampleRace.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pb_start_clicked();
+
+private:
+    void StartRace();
+
+    Ui::MainWindow *ui;
+
+    uint32_t number = 0;
+    uint32_t countFinish = 0;
+
+    QMutex m;
+
+    Controller *race1;
+    Controller *race2;
+
+    ExampleRace *concurRace1;
+    ExampleRace *concurRace2;
+};
+
+#endif // MAINWINDOW_H
